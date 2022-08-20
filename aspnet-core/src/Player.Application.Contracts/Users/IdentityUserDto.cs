c3576cba1d3dd3da19a180c5aeba964baa584d5f
anpt <anpt@AN-PT>
2022-08-20 19:17:55 +0700

    commit

---
+++ aspnet-core/src/Player.Application.Contracts/Users/IdentityUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Player.Users
+{
+    public class IdentityUserDto
+    {
+        public string Name { get; set; }
+        public string Email { get;set; }
+        public string PhoneNumber { get; set; }
+        public string Sex { get; set; }
+        public string Skype { get; set; }
+    }
+}
